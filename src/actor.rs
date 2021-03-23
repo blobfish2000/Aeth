9c8907ef74e6167322ab6c9b77adef3c039b92b3
blobfish2000 <blobfish2000@gmail.com>
2021-03-22 22:54:03 -0700

    removed bad ideas

--- src/actor.rs
+++ src/actor.rs
@@ -1,18 +1,1 @@
-pub mod actor {
-
-    pub enum Race {
-        Human,
-        Elf,
-    }
-
-    pub struct Actor<'a> {
-        pub next_update: u32,
-        pub granularity: u32,
-        pub name: String,
-        pub quantity: u32,
-        pub age: u32,
-        pub race: Race,
-        pub job: String,
-        pub dependants: Vec<&'a Actor<'a>>,
-    }
-}
+pub mod actor {}
